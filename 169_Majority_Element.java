63b5fcb84e48f6f4069a1e53aaa453f578af5fbc
jaikaran109 <jaik05581@gmail.com>
2026-01-13 15:09:59 +0530

    Update 169_Majority_Element.java

--- 169_Majority_Element.java
+++ 169_Majority_Element.java
@@ -1,23 +1,70 @@
-class Solution {
-    public int majorityElement(int[] arr) {
-        int val = arr[0];
-        int count = 1;
+package Array;
 
-        //Booyre More Voting Algorithm
-        for(int i = 1 ; i < arr.length ; i++){
-            if(arr[i] == val){
-                count++;
-            }else{
-                if(count > 0){
-                    count--;
-                }
-                else{
-                    val = arr[i];
-                    count++;
-                }
-            }
-        }
-        return val;
 
-    }
+import java.util.*;
+public class LC169_Majority_Element {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int size = input.nextInt();
+		int[] arr = new int[size];
+		for(int i = 0 ; i < size ; i++) {
+			arr[i] = input.nextInt();
+		}
+		System.out.print(BoyerMooreVotingAlgorithm(arr));
+	}
+	static int BoyerMooreVotingAlgorithm(int[] nums) {
+	    int val = nums[0];
+	    int count = 1;
+
+	    for (int i = 1; i < nums.length; i++) {
+	        if (nums[i] == val) {
+	            count++;
+	        } else {
+	            count--;
+	            if (count == 0) {
+	                val = nums[i];
+	                count = 1;
+	            }
+	        }
+	    }
+	    return val;
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// LEET CODE
+// class Solution {
+//     public int majorityElement(int[] arr) {
+//         int val = arr[0];
+//         int count = 1;
+
+//         //Booyre More Voting Algorithm
+//         for(int i = 1 ; i < arr.length ; i++){
+//             if(arr[i] == val){
+//                 count++;
+//             }else{
+//                 if(count > 0){
+//                     count--;
+//                 }
+//                 else{
+//                     val = arr[i];
+//                     count++;
+//                 }
+//             }
+//         }
+//         return val;
+
+//     }
+// }
